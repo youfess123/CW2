06ceb70e63bbc64f69c310271bc8f03ace0e63c4
youfe <youfess2003@gmail.com>
2024-12-27 04:16:19 +0000

    vader

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <vector>
 #include <sstream>
 
@@ -13,7 +14,7 @@ struct Order {
 
 std::vector<Order> processFile(const std::string &fileName) {
     std::ifstream inputFile(fileName);
-    std::vector<Order> orders;
+    std::vector<Order> pendingOrders;
     int priority=1;
 
     float previousTransactionPrice;
@@ -28,30 +29,58 @@ std::vector<Order> processFile(const std::string &fileName) {
             if (stringStream >> order.price){
             }
             order.priority = priority++;
-            orders.push_back(order);
+            pendingOrders.push_back(order);
+        }
+    }
+
+    std::vector<Order> buyOrders;
+    std::vector<Order> sellOrders;
+
+    for(const auto &order : pendingOrders) {
+        if (order.orderType=='B') {
+            buyOrders.push_back(order);
+        }else {
+            sellOrders.push_back(order);
         }
     }
 
     inputFile.close();
-    return orders;
+    return pendingOrders;
+
+}
+void printOrder(const std::vector<Order> &buyOrders, const std::vector<Order> &sellOrders, float lastTransactionPrice) {
+
+    std::cout << "Last trading price: " << std::fixed << std::setprecision(2) << lastTransactionPrice << "\n";
+    std::cout << "Buy                       Sell\n";
+    std::cout << "------------------------------------------\n";
+    
 
 }
 
-void processOrder(const std::vector<Order> &orders) {
-    for (const auto& order : orders) {
-        std::cout << "Order ID: " << order.orderId << ", Type: " << order.orderType
-                  << ", Size: " << order.orderSize << ", Price: " << order.price
-                  << ", Priority: " << order.priority << std::endl;
+void processOrder(const std::vector<Order> &pendingOrders) {
+    std::vector<Order> executedOrders;
+    std::vector<Order> unexecutedOrders;
+
+    for (const auto& order : executedOrders) {
+        std::string type;
+        if(order.orderType =='B') {
+            type="purchased";
+        }else {
+            type="sold";
+        }
+        std::cout << "order " << order.orderId << " " << order.orderSize << " shares "+type+" at price " << order.price << std::endl;
+    }
+    for (const auto& order : unexecutedOrders) {
+        std::cout << "order " << order.orderId +" " << order.orderSize + " shares unexecuted" << std::endl;
     }
 }
 
 int main() {
     std::string fileName;
-    std::cout << "Enter the file name: ";
+    std::cout << "Enter the file name: "<< std::endl;
     std::cin>>fileName;
-    processFile(fileName);
-    std::vector<Order> orders = processFile(fileName);
-    processOrder(orders);
+    std::vector<Order> pendingOrders = processFile(fileName);
+    processOrder(pendingOrders);
 
     return 1;
 
